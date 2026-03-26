36996556468eb0df4440bc74aee237b63d4818fb
Daniel Alejandro Gonzalez Morales <alejandrodaniel1525@gmail.com>
2026-03-26 04:13:01 +0000

    subo cola

--- cola/cola.c
+++ cola/cola.c
@@ -1,48 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 #include "cola.h"
+#include "../lista/lista.h"
 
-Cola* cola_crear()
-{
+Cola* cola_crear() {
     return lista_crear();
 }
 
-int cola_vacia(Cola* cola)
-{
-    /*
-    TODO
-    usar vaciar_lista
-    */
+bool cola_vacia(Cola* cola) {
+    return lista_vacia(cola);
 }
 
-void cola_enqueue(Cola* cola, int dato)
-{
-    /*
-    TODO
-    usar lista_insertar_tail
-    */
+void cola_enqueue(Cola* cola, int dato) {
+    lista_insertar_tail(cola, dato);
 }
 
-int cola_dequeue(Cola* cola)
-{
-    /*
-    TODO
-    usar lista_eliminar_head
-    */
-    return -1;
+int cola_dequeue(Cola* cola) {
+    return lista_eliminar_head(cola);
 }
 
-int cola_frente(Cola* cola)
-{
-    /*
-    TODO
-    regresar dato del head
-    */
-    return -1;
+int cola_frente(Cola* cola) {
+    if(cola_vacia(cola)) return -1;
+
+    return cola->head->dato;
 }
 
-void cola_destruir(Cola* cola)
-{
-    /*
-    TODO
-    usar lista_destruir
-    */
+int cola_fin(Cola* cola) {
+    if(cola_vacia(cola)) return -1;
+
+    return cola->tail->dato;
 }
+
+void cola_destruir(Cola* cola) {
+    lista_destruir(cola);
+}
+    
--- cola/cola.h
+++ cola/cola.h
@@ -2,12 +2,13 @@
 #define COLA_H
 
 #include "../lista/lista.h"
+#include <stdbool.h>
 
 typedef Lista Cola;
 
 Cola* cola_crear();
 
-int cola_vacia(Cola* cola);
+bool cola_vacia(Cola* cola);
 
 /*
 TODO
@@ -26,6 +27,8 @@ TODO
 regresar frente (head)
 */
 int cola_frente(Cola* cola);
+
+int cola_fin(Cola* cola);
 
 void cola_destruir(Cola* cola);
 
