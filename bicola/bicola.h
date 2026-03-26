4d5b992fa7549e969c5d5ece565d9de2cc57f237
Daniel Alejandro Gonzalez Morales <alejandrodaniel1525@gmail.com>
2026-03-26 04:10:20 +0000

    subo bicola

--- bicola/bicola.h
+++ bicola/bicola.h
@@ -1,26 +1,28 @@
 #ifndef BICOLA_H
 #define BICOLA_H
+
 #include "../lista/lista.h"
 #include "../cola/cola.h"
 #include <stdbool.h>
 
 typedef Lista Bicola;
 
-Bicola * cola_crear();
+Bicola* bicola_crear();
 
-bool bicola_vacia(Cola*c);
+bool bicola_vacia(Bicola *c);
 
-void bicola_enqueue_head( Cola *c int dato);
+void bicola_enqueue_head(Bicola *c, int dato);
 
-void bicola_enqueue_tail( Cola *c int dato);
+void bicola_enqueue_tail(Bicola *c, int dato);
 
-void bicola_denqueue_head( Cola *c int dato);
+int bicola_denqueue_head(Bicola *c);
 
-void bicola_denqueue_tail( Cola *c int dato);
+int bicola_denqueue_tail(Bicola *c);
 
-int bicola_enqueue_head(Cola *c);
+int bicola_head(Bicola *c);
 
-int bicola_enqueue_tail(cola *c);
+int bicola_tail(Bicola *c);
 
-void bicola_destruir(cola *c);
+void bicola_destruir(Bicola *c);
+
 #endif
